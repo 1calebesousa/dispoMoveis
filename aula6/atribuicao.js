c248f8de887881e6d08148c73152097ad35b21ab
Calebe Alves de Sousa <calebinhuusousa@gmail.com>
2026-03-25 11:07:10 -0300

    aula6

---
+++ aula6/atribuicao.js
@@ -0,0 +1,14 @@
+//operadores de atribuição
+let n3 = 20;
+n3 += 15; //n3 = n3 + 15
+console.log(n3);
+n3 -= 15; // n3 = n3 - 15
+console.log(n3);
+n3 *= 15; // n3 = n3 * 15
+console.log(n3);
+n3 /= 15; //n3 = n3 / 15
+console.log(n3);
+n3 %= 15; //n3 = n3 % 15
+console.log(n3);
+n3 **= 15; //n3 = n3 ** 15
+console.log(n3);
